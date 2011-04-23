799e280ebc946f645b37850f3d1a9ff78e3f153c
Greg Liras <gregliras@gmail.com>
2011-04-23 18:19:27 +0300

    asdf'

--- askhsh1/c_programm/magic.c
+++ askhsh1/c_programm/magic.c
@@ -1,92 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <time.h>
 #include <string.h>
-static unsigned int addB(unsigned int n1,unsigned int n2,unsigned int b)
+#include "convert.h"
+
+unsigned int buffer[64],number2[64],number[64];
+
+
+static void addB(unsigned int n,unsigned int b)
 {
-  unsigned int ans=0,i=0,c=0,buff=0;
-  while(n1!=0 || n2!=0)
+  unsigned int buff,c=1;
+  int i = (int) n-1;
+  for (;i>=0;i--)
   {
-    buff=n1%10+n2%10+c;
-    ans+=(buff%b)*pow(10,i++);
+    buff=number[i]+number2[i]+c;
+    number[i]=buff%b;
     c=buff/b;
-    n1=n1/10;
-    n2=n2/10;
   }
-  return ans;
 }
-static unsigned int complementB(unsigned int n,unsigned int b)
+static void complementB(unsigned int n,unsigned int b)
 {
-  unsigned int buff=0,i=0,ans=0;
-  while(n!=0)
+  unsigned int i;
+  for(i=0;i<n;i++)
   {
-    buff=n%10;
-    n=n/10;
-    ans+=(b-1-buff)*pow(10,i++);
-  }
-  return ans;
-}
-static unsigned int sortNum(long n) {
-  while (true) {
-    long a = n % 10, p = 9;
-    bool s = false;
-    for (long r = n / 10; r; r/= 10) {
-      long b = r % 10;
-      if (a > b) {
-        n -= p * (b - a);
-        s = true;
-      } else a = b;
-      p *= 10;
-    }
-    if (!s) return n;
+    number[i]=b-1-number[i];
   }
 }
-static unsigned int subtract(unsigned int n1,unsigned int n2, unsigned int b)
+
+
+static int compare (const void *a,const void *b)
 {
-  unsigned int i=0, ans=0;
-  n2=complementB(n2,b);
-  ans=addB(n1,n2,b);
-  ans=addB(ans,1,b);
-  return ans;
+  return ( *(int*)a - *(int*)b );
 }
+static int compare2 (const void *a,const void *b)
+{
+  return ( *(int*)b - *(int*)a );
+}
+static void bigrandom_X(unsigned int n,unsigned int b)
+{
+  unsigned int k = 0,i;
+  // First digit must be non-zero:
+  srand(time(NULL));
+  do
+  {
+    k = rand() % b;
+  } while(k == 0);
+
+  number[0]=k;
+  for(i = 1; i < n; i++)
+  {
+    k = rand() % b;
+    number[i]=k;
+  }
+  for (;i<64;i++)
+  {
+    number[i]=0;
+  }
+}
+
+
 static unsigned int magic(unsigned int n, unsigned int b)
 {
+  unsigned int i;
+  //, random_start;
+  //random_start=bigrandom_Base_X(n,b);
+  bigrandom_X(n,b);
+  /*  for(i=0;i<64;i++)
+      {
+      printf("%u ",number[i]);
+      }
+      */
+  while(1)
+  {
+    //printf("UnSorted:\n");
+    for (i=0;i<n;i++)
+    {
+      if(buffer[i]!=number[i]) break;
+      else return 1;
+    }
+    for(i=0;i<64;i++)
+    {
+      buffer[i]=number[i];
+      //printf("%u ",number[i]);
+    }
+    qsort(number,n,sizeof(unsigned int),compare);
+    //printf("\nSorted:\n");
+
+    for(i=0;i<64;i++)
+    {
+      buffer[i]=number[i];
+      number2[i]=number[i];
+      //      printf("%u ",number[i]);
+    }
+    qsort(number2,n,sizeof(unsigned int),compare2);
+    //printf("\nSecond\n");
+    /*
+       for(i=0;i<64;i++)
+       {
+    //    number[i]=number[i]-number2[i];
+    printf("%u ",number2[i]);
+    }
+    printf("\nComplement base %u\n",b-1);
+    */
+    complementB(n,b);
+    /*    for(i=0;i<n;i++)
+          {
+          printf("%u ",number[i]);
+          }
+          */
+    addB(n,b);
+    /*        printf("\nResult of sub:\n");
+              for(i=0;i<64;i++)
+              {
+              printf("%u ",number[i]);
+              }
+              printf("\n");
+              */
+  }
+  printf("\nSame one\n");
   return 0;
 }
-static unsigned int convert_to_base_10(unsigned int n,unsigned int b)
-{
-  unsigned int i = 0;
-  unsigned int result = 0;
-  while (n>0)
-  {
-    result+=(n%10)*pow(b,i);
-    n=n/10;
-    i++;
-  }
-  return result;
-}
-/*
- * convert to base 10 calculate convert to base b display result
- *
- *
- static unsigned int sub(unsigned int big,unsigned int small, unsigned int b)
- {
- unsigned int
- }
-
-
-*/
-
 int main(int argc, char* argv[])
 {
   unsigned int b;
   unsigned int n;
-
+  unsigned int i;
+  unsigned int answer=0;
   if (argc != 3) {
     printf("Usage: magic n b\n"); return 1;
   }
   sscanf(argv[1], "%u", &n);
   sscanf(argv[2], "%u", &b);
-  printf("%u\n", subtract(n,n,b));
+  if(magic(n,b)==1)
+  {
+  for(i=0;i<n;i++)
+  {
+    answer+=number[i]*pow(b,n-i-1);
+  }
+  printf("%u",answer);
+  }
+  else printf("DAMN\n");
+  //printf("\n", magic(n,b));
+  printf("\n");
   return 0;
 }
