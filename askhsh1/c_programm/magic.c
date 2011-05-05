34b2b25085c911dfa7fd59f29187ed5a4d36e9fa
Greg Liras <gregliras@gmail.com>
2011-05-05 19:33:23 +0300

    speed up

--- askhsh1/c_programm/magic.c
+++ askhsh1/c_programm/magic.c
@@ -3,13 +3,13 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
-
-unsigned int toprint[86],magi[64],buffer[64],number2[64],number[64];
+#include <gmp.h>
+unsigned int magi[64],buffer[64],number2[64],number[64];
 
 
 static void next_test(unsigned int n,unsigned int b)
 {
-  unsigned int buff,c=1,ndiv2=n/2-1;
+  unsigned int buff,c=1,ndiv2=(n%2==1) ? n/2 : n/2-1;
   int i = (int) n-1;
   for (;i>=0;i--)
   {
@@ -20,55 +20,32 @@ static void next_test(unsigned int n,unsigned int b)
   }
   for(i=0;i<(int)n-1;i++)
   {
+    number[i]=buffer[i];
     if(buffer[i+1]<buffer[i])
     {
       buffer[i+1]=buffer[i];
     }
-      }
+  }
+  number[n-1]=buffer[n-1];
   if(buffer[ndiv2]==1)
   {
     printf("0\n");
     exit(0);
   }
 }
-static void add_to_printbuffer(unsigned int num)
-{
-  int i,j;
-  unsigned int buff,c=0;
-  for (i=85,j=63;j>=0;j--,i--)
-  {
-    buff=toprint[i]+number[j];
-    toprint[i]=buff;
-    num/=10; 
-  }
-}
 static void print_number(unsigned int n,unsigned int b)
 {
-  unsigned long long int n1=0,n2=0;
-  unsigned int i,ndiv2=n/2;
-  if (b<10)
+  mpz_t bigint,rop;
+  mpz_init(bigint);
+  mpz_init(rop);
+  unsigned long int i;
+  for(i=0;i<n;i++)
   {
-    for (i=0;i<n;i++)
-    {
-      n1+=number[n-i-1]*pow(b,i);
-    }
-    printf("%llu\n",n1);
+      mpz_ui_pow_ui(rop,b,i);
+      mpz_mul_ui(rop,rop,number[n-i-1]);
+      mpz_add(bigint,bigint,rop); 
   }
-  else if (b==10)
-  {
-    for (i=0;i<n;i++)
-    {
-      printf("%u",number[i]);
-    }
-    printf("\n",n1);
-  }
-  else
-  {
-    for(i=0;i<n;i++)
-    {
-       
-    }
-  }
+  gmp_printf("%Zd\n",bigint);
 }
 static void addComplement(unsigned int n,unsigned int b)
 {
@@ -83,10 +60,11 @@ static void addComplement(unsigned int n,unsigned int b)
 }
 static void complementB(unsigned int n,unsigned int b)
 {
-  unsigned int i;
-  for(i=0;i<n;i++)
+  int i;
+  for(i = (int) n ; i >= 0;i--)
   {
-    number[i]=b-1-number[i];
+    number2[n-i-1] = number[i];
+    number[i] = b-1-number[i];
   }
 }
 
@@ -95,38 +73,9 @@ static int compare (const void *a,const void *b)
 {
   return ( *(int*)a - *(int*)b );
 }
-/*
-static void  extra_test( int n, int b)
-{
-  int i,diff1,diff2,ndiv2=(n/2)-1;
-  for (i=0;i<ndiv2;i++)
-  {
-    //    printf("number[i] %u\n",number[i]);
-    diff1=buffer[n-i-1]-buffer[i];
-    diff2=buffer[n-i-2]-buffer[i+1];
-    if(diff1<diff2)
-    {
-      printf("failed extra test\n");
-      //  printf("%u - %u %u - %u\n\n",number[n-i-1],number[i],number[n-i-2],number[i+1]);
-      next_test(n,b);
-      extra_test(n,b);
-    }
-  }
-}
-*/
 static int ismagic(unsigned int n,unsigned int b)
 {
   unsigned int i;
-//  extra_test(n,b);
-  for(i=0;i<n;i++)
-  {
-    number[i]=buffer[i];
-  }
-  for(i=0;i<n;i++)
-  {
-//    printf("%u ",number[i]);
-    number2[i]=number[n-i-1];
-  }
   complementB(n,b);
   addComplement(n,b);
   for(i=0;i<n;i++)
@@ -134,15 +83,6 @@ static int ismagic(unsigned int n,unsigned int b)
     magi[i]=number[i];
   }
   qsort(number,n,sizeof(unsigned int),compare);
-/*  if (extra_test(n,b)==1) 
-  {
-    return 0;
-  }*/
-  for(i=0;i<n;i++)
-  {
-//    printf("%u ",number[i]);
-    number2[i]=number[n-i-1];
-  }
   complementB(n,b);
   addComplement(n,b);
   for (i=0;i<n;i++)
@@ -168,8 +108,6 @@ int main(int argc, char* argv[])
 {
   unsigned int b;
   unsigned int n;
-  unsigned int i;
-  //unsigned long long int answer=0;
   if (argc != 3) 
   {
     printf("Usage: magic b n\n"); 
@@ -179,16 +117,7 @@ int main(int argc, char* argv[])
   sscanf(argv[2], "%u", &n);
   if(magic(n,b)==0)
   {
-    for(i=0;i<n;i++)
-    {
-      printf("%u ",number[i]);
-      //answer+=number[i]*pow(b,n-i-1);
-    }
-    //printf("\n%llu",answer);
-    print_number(n,b);
+   print_number(n,b);
   }
-  else printf("DAMN\n");
-  //printf("\n", magic(n,b));
-  printf("\n");
   return 0;
 }
