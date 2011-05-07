bdd2e0406c5b47fadd3dd7e9691191bdb530b66f
Greg Liras <gregliras@gmail.com>
2011-05-07 18:06:40 +0300

    site_names_done

---
+++ askhsh2/c_programm/site_names.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct trie
+{
+  unsigned int level;
+  unsigned int counter;
+  unsigned int children;
+  char character;
+  struct trie * child[53];
+};
+typedef struct trie tri;
+unsigned int max;
+
+void fill(tri * head,char i)
+{
+  tri *current=head;
+  tri *newchild;
+  unsigned int children,buf;
+  while(i!='\n')
+  {
+    for(children=current->children;children>0;children--)
+    {
+      if (current->child[children-1]->character==i)
+      {
+        current->child[children-1]->counter++;
+        current=current->child[children-1];
+        break;
+      }
+    }
+    if (children==0)
+    {
+      newchild=(tri *)calloc(1,sizeof(tri));
+      current->child[current->children]=newchild;
+      newchild->level=current->level+1;
+      newchild->character=i;
+      newchild->counter=1;
+      current->children++;
+      current=newchild;
+    }
+    i=getchar();
+  }
+  buf=current->level*current->counter;
+  max = (max>buf)?max:buf;
+}
+
+int main(int argc, char **argv)
+{
+  char i;
+  tri * head;
+  head=(tri *)calloc(1,sizeof(tri));
+  while((i=getchar())!=EOF)
+  {
+    fill(head,i); 
+  }
+  printf("%u\n",max);
+  return 0;
+}
